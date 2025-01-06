7b11e55a778ca40bf54d02b3258f7204e83c612b
Xinkai Du <xinkai.du.xd@gmail.com>
2025-01-06 02:12:31 +0100

    Initial commit

---
+++ server.R
@@ -0,0 +1,243 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  # ----- data source -----
+  
+  dt_react <- reactive({
+    
+    if (input$dt_mis_condition == "dt_null"){
+      dt[[input$dt_mis_condition]][condition == input$true_model,]
+    } else {
+      dt[[input$dt_mis_condition]][condition == input$true_model & n_node_factor == input$n_nodes,]
+    }
+    
+  })
+  
+  
+  # ----- boxplots -----
+  
+  boxplot_react <- eventReactive(input$draw_box_plot, {
+    
+    if (input$dt_mis_condition == "dt_null") {
+      box_plotter(dt_react(), metric = input$fit_index, 
+                  x_facet = "n_node_factor", y_facet = "n_time_factor", 
+                  color = "type") + guides(fill=guide_legend(title="Model type"))
+    } else if (input$dt_mis_condition == "dt_mean") {
+      box_plotter(dt_react(), metric = input$fit_index, 
+                  x_facet = "type", y_facet = "n_time_factor", 
+                  color = "mean_trend_factor") + guides(fill=guide_legend(title="Mean trends"))
+    } else if (input$dt_mis_condition %in% c("dt_rt", "dt_nt")) {
+      box_plotter(dt_react(), metric = input$fit_index, 
+                  x_facet = "type", y_facet = "n_time_factor", 
+                  color = "rewire_temporal_factor") + guides(fill=guide_legend(title="Rewiring probability"))
+    } else {
+      box_plotter(dt_react(), metric = input$fit_index, 
+                  x_facet = "type", y_facet = "n_time_factor", 
+                  color = "rewire_contemporaneous_factor") + guides(fill=guide_legend(title="Rewiring probability"))
+    }
+    
+  })
+  
+  output$boxplot <- renderPlot({boxplot_react()})
+  
+  
+
+  # ----- line plots -----
+  
+  # *--- calculate rejection rates ---
+  rej_react <- eventReactive(input$draw_line_plot, {
+    
+    # obtain data  
+    data <- dt_react()
+    
+    # calculate rejection rate of absolute indices
+    rej <- data[, (sum(RMSEA > input$RMSEA_cut) / .N), by = c(conditions_ls[[input$dt_mis_condition]])]
+    setnames(rej, "V1", paste0("RMSEA>", input$RMSEA_cut))
+    
+    # cutoffs for incremental indices
+    cutoffs <- c(0.05, input$NFI_cut, input$PNFI_cut, input$TLI_cut,
+                 input$RFI_cut, input$IFI_cut, input$RNI_cut, input$CFI_cut)
+    
+    # var names to identify rej rates
+    rej_var <- paste0(metrics[-9], "<", cutoffs)
+    
+    # calculate rejection rates of incremental indices
+    for (i in 1:length(rej_var)) {
+      rej[[rej_var[i]]] <- data[, (sum(get(metrics[i]) < cutoffs[i]) / .N), by = c(conditions_ls[[input$dt_mis_condition]])][, V1]
+    }
+    
+    rej <- rej[type == input$model_type,]
+    
+    # make long-format
+    longer <- melt(rej, id.vars = conditions_ls[[input$dt_mis_condition]])
+    
+    return(longer)
+    
+  })
+  
+  # *--- draw plot --- 
+  
+  line_plot_react <- eventReactive(input$draw_line_plot, {
+    
+    if (input$dt_mis_condition == "dt_null") {
+      line_plotter(data = rej_react(), x = "n_person_factor", y = "value", color = line_colors,
+                   color_var = "variable", xfacet = "n_node_factor", yfacet = "n_time_factor")
+    } else if (input$dt_mis_condition == "dt_mean") {
+      line_plotter(data = rej_react(), x = "n_person_factor", y = "value", color = line_colors,
+                   color_var = "variable", xfacet = "mean_trend_factor", yfacet = "n_time_factor")
+    } else if (input$dt_mis_condition %in% c("dt_rt", "dt_nt")) {
+      line_plotter(data = rej_react(), x = "n_person_factor", y = "value", color = line_colors,
+                   color_var = "variable", xfacet = "rewire_temporal_factor", yfacet = "n_time_factor")
+    } else {
+      line_plotter(data = rej_react(), x = "n_person_factor", y = "value", color = line_colors,
+                   color_var = "variable", xfacet = "rewire_contemporaneous_factor", yfacet = "n_time_factor")
+    }
+    
+  })
+  
+  # output the line plot
+  output$line_plot <- renderPlot({line_plot_react()})
+  
+  
+  # ----- ANCOVA table -----
+
+  # *--- data source for ANCOVA ---
+
+  dt_ancova_react <- reactive({
+      if (input$dt_mis_condition %in% c("dt_rt", "dt_rc")) {
+        dt[[input$dt_mis_condition]][condition == input$true_model & type == "Confirmatory",][, type := NULL]
+      } else {
+        dt[[input$dt_mis_condition]][condition == input$true_model,]
+      }
+  })
+  
+  # *--- ANCOVA ---
+  # conditions
+  tab_react <- eventReactive(input$make_ANOVA_table, {
+    
+    # load data
+    data <- dt_ancova_react()
+    
+    data[[1]] <- data[[1]] %>% as.character %>% 
+      sub(pattern = ".* = ", replacement = "") %>% as.numeric
+    
+    # conditions (mismatch conditions don't have type as predictor)
+    if (input$dt_mis_condition %in% c("dt_rt", "dt_rc")) {
+      conditions <- conditions_ls[[input$dt_mis_condition]][-2]
+    } else {
+      conditions <- conditions_ls[[input$dt_mis_condition]]
+    }
+
+    # formulas
+    formulas <- paste(metrics[-1], "~", paste(conditions, collapse = " + "))
+    
+    # result list
+    ANCOVA = list()
+    
+    # ancova series
+    ANCOVA <- lapply(formulas, function(f) {
+      f <- as.formula(f)
+      aov.fit <- sjstats::anova_stats(aov(f, data = data)) %>% as.data.table
+      aov.fit <- aov.fit[, c("partial.etasq", "term")]
+    })
+    
+    # rename list dimensions
+    names(ANCOVA) <- metrics[-1]
+    
+    # bind results
+    ANCOVA <- rbindlist(ANCOVA, idcol = "Metrics")
+    
+    # just keep the first identifier of each table
+    if (input$dt_mis_condition %in% c("dt_rt", "dt_rc")) {
+      
+      ANCOVA <- ANCOVA[, lapply(.SD, as.character)][
+        # round numbers with roundif, except for the residual row
+        (1:nrow(ANCOVA))[-seq(5, nrow(ANCOVA), by = 5)], 
+        partial.etasq := lapply(.SD, roundif), .SDcols = "partial.etasq"][
+          # mark p < .001
+          , partial.etasq := lapply(.SD, function(x) {
+            ifelse(as.numeric(x) < .001, "< .001", x)
+          }), .SDcols = "partial.etasq"]
+                       
+      
+    } else {
+      
+      ANCOVA <- ANCOVA[, lapply(.SD, as.character)][
+        # round numbers with roundif, except for the residual row
+        (1:nrow(ANCOVA))[-seq(6, nrow(ANCOVA), by = 6)], 
+        partial.etasq := lapply(.SD, roundif), .SDcols = "partial.etasq"][
+          # mark p < .001
+          , partial.etasq := lapply(.SD, function(x) {
+            ifelse(as.numeric(x) < .001, "< .001", x)
+          }), .SDcols = "partial.etasq"]
+      
+    }
+    
+    # store the partial eta squares
+    p.etasq <- dcast(ANCOVA, formula = Metrics ~ term, value.var = "partial.etasq")     
+  
+    # remove unncessary columns & order to find the index that was the most sensitive to mean_trends and least to covariates
+    if (input$dt_mis_condition == "dt_mean") {
+      
+      p.etasq <- p.etasq[, Residuals := NULL][
+        order(-mean_trend_factor,
+              n_node_factor, 
+              n_person_factor, 
+              n_time_factor)] 
+        
+    } else if (input$dt_mis_condition %in% c("dt_rt", "dt_nt")) {
+      
+      p.etasq <- p.etasq[, Residuals := NULL][
+        order(-rewire_temporal_factor,
+              n_node_factor, 
+              n_person_factor, 
+              n_time_factor)]
+      
+    } else {
+      
+      p.etasq <- p.etasq[, Residuals := NULL][
+        order(-rewire_contemporaneous_factor,
+              n_node_factor, 
+              n_person_factor, 
+              n_time_factor)]
+      
+    }
+    
+    # reorder
+    if (input$dt_mis_condition == "dt_rt") {
+      p.etasq <- p.etasq[,c("Metrics","rewire_temporal_factor", "n_node_factor", 
+                 "n_person_factor", "n_time_factor")]
+    } else if (input$dt_mis_condition == "dt_rc") {
+      p.etasq <- p.etasq[,c("Metrics","rewire_contemporaneous_factor", "n_node_factor", 
+                 "n_person_factor", "n_time_factor")]
+    } else if (input$dt_mis_condition == "dt_nt") {
+      p.etasq <- p.etasq[,c("Metrics","rewire_temporal_factor", "n_node_factor", 
+                            "n_person_factor", "n_time_factor", "type")]
+    } else if (input$dt_mis_condition == "dt_nc") {
+      p.etasq <- p.etasq[,c("Metrics","rewire_contemporaneous_factor", "n_node_factor", 
+                            "n_person_factor", "n_time_factor", "type")]
+    } else {
+      p.etasq <- p.etasq[,c("Metrics","mean_trend_factor", "n_node_factor", 
+                            "n_person_factor", "n_time_factor", "type")]
+    }
+    
+    # assign more informative colnames 
+    if (input$dt_mis_condition == "dt_mean") {
+      colNames <- c("Mean trend", "Number of nodes", "Sample size", "Number of waves", "Model type")
+    } else if (input$dt_mis_condition %in% c("dt_rt", "dt_rc")) {
+      colNames <- c("Rewire probability", "Number of nodes", "Sample size", "Number of waves")
+    } else {
+      colNames <- c("Rewire probability", "Number of nodes", "Sample size", "Number of waves", "Model type")
+    }
+
+    colnames(p.etasq)[-1] <- colNames
+    
+    return(p.etasq)
+    
+  }) # end eventReact make_ancova_table
+  
+  output$ancova_table <- renderTable({tab_react()})
+  
+}) # end server
+
---
+++ ui.R
@@ -0,0 +1,216 @@
+library(shiny)
+
+# define ui
+navbarPage("Confirmatory Network Analysis Fit Measures", theme = shinytheme("cosmo"),
+           
+           # ----- introduction ----- 
+           tabPanel("Introduction",
+                    fluidPage(
+                      
+                      # ----- title -----
+                      
+                      fluidRow(
+                        column(width = 12,
+                               tags$div(
+                                 tags$strong("Confirmatory Network Analysis Fit Measures",
+                                             style = "font-size:24px;")
+                               ),
+                               tags$div(
+                                 HTML(paste(
+                                   "This app is to display the results for paper ",
+                                   "Du, X., Skjerdingstad, N., Freichel, R., ",
+                                   "Ebrahimi, O. V., Hoekstra, R. H. A., & Epskamp, S. (",
+                                   tags$a(
+                                     href = "https://osf.io/d76ab",
+                                     "under review"),
+                                   "). Moving from exploratory to confirmatory network analysis: ",
+                                   "An evaluation of SEM fit indices and cutoff values in network psychometrics.",
+                                   sep = ""))
+                               )
+                        ) # end column  
+                      ), # end fluidRow for title
+                      
+                      
+                      
+                      # ----- instructions -----
+                      
+                      h4("How to use this app"),
+                      
+                      # *--- three options --- 
+                      tags$div("You can choose from"),
+                      tags$ul(
+                        tags$li(tags$b("Boxplots:"),
+                                "Displays how fit measures change in response to", 
+                                "different levels of misspecifications",
+                                "under various sample characteristics."),
+                        tags$li(tags$b("Lineplots:"), "Displays the rejection rates of", 
+                                "different cutoff values in response to different levels",
+                                "of misspecifications under various sample characteristics."),
+                        tags$li(tags$b("ANCOVA tables:"),
+                                "Display the effect sizes that indicate the sensitivity",
+                                "of fit measures to different types of misspecifications",
+                                "and their vulnerability to sample characteristics.")
+                      ), # end tab introduction
+                      
+                      # *--- true model setups --- 
+                      tags$div(
+                        HTML(paste("Then you choose for which true model setup you display the results.",
+                                   "The default is to display the results of the original study.",
+                                   "In response to the reviewer's feedback,",
+                                   "we also display the results for two alternative true model setups",
+                                   "to showcase the robustness of our results.",
+                                   "In total, there are three setups"))),
+                      tags$ul(
+                        tags$li(tags$b("Original (propPos = 0.8 & nei = 1)"), 
+                                "In the original study,",
+                                "we set the proportion of positive edges to be 0.5", 
+                                "and number of neighbors for each node to be 1"),
+                        tags$li(tags$b("Setting propPos = 0.8:"), 
+                                "A reviewer suggested to check the results",
+                                "if we set the proportion of positive edges in the networks to be 0.8"),
+                        tags$li(tags$b("Setting nei = 2:"), 
+                                "Check the results if we change number of neighbors for each node to be 2")
+                      )
+                    ),
+                    
+                    # *--- contact information --- 
+                    hr(),
+                    HTML(paste(
+                      "Please contact the app author Xinkai Du for questions or error reporting:",
+                      tags$a(href = "mailto:xinkai.du.xd@gmail.com",
+                             "xinkai.du.xd@gmail.com")
+                    ))
+                      
+           ), # end tab: Introduction
+           
+           
+           # ----- boxplots ----- 
+           
+           tabPanel("Boxplots",
+                    sidebarLayout(
+                      sidebarPanel(
+                        actionButton("draw_box_plot", "Create plot"),
+                        hr(),
+                        
+                        # *--- choose dataset --- 
+                        selectInput("dt_mis_condition", "Choose the misspecification condition", 
+                                    choices = c("Simulation Study (1): Null model" = "dt_null", 
+                                                "Simulation Study (2): Mean trends" = "dt_mean", 
+                                                "Simulation Study (3): Misspecified temporal networks" = "dt_rt",
+                                                "Simulation Study (4): Misspecified contemporaneous networks" = "dt_rc",
+                                                "Simulation Study (5): Non-stationary temporal networks" = "dt_nt",
+                                                "Simulation Study (6): Non-stationary contemporaneous networks" = "dt_nc")),
+                        
+                        # *--- choose true model ---
+                        selectInput("true_model", "Choose the true model setup",
+                                    choices = c("nei = 1 & propPos = 0.5",
+                                                "nei = 1 & propPos = 0.8",
+                                                "nei = 2 & propPos = 0.5")),
+                        
+                        # *--- fit measure ---
+                        selectInput("fit_index", "The fit measure you wish to display results for",
+                                    choices = metrics[-1]),
+                        
+                        # *--- choose sample characteristics --- 
+                        conditionalPanel(
+                          condition = "input.dt_mis_condition != 'dt_null'",
+                          selectInput("n_nodes", "Choose the number of nodes",
+                                      choices = c("6 nodes" = "p = 6",
+                                                  "12 nodes" = "p = 12"))
+                        )
+                        
+                      ), # end sidebar panel
+                      
+                      # *--- plots display ---
+                      mainPanel(
+                        plotOutput("boxplot")
+                      ) # end main panel
+                    ) # end sidebar layout
+           ), # end tab: Boxplots
+           
+           
+           # ----- line plots -----
+           
+           tabPanel("Line Plots",
+                    sidebarLayout(
+                      sidebarPanel(
+                        actionButton("draw_line_plot", "Create plot"),
+                        hr(),
+                        
+                        # *--- choose dataset --- 
+                        selectInput("dt_mis_condition", "Choose the misspecification condition", 
+                                    choices = c("Simulation Study (1): Null model" = "dt_null", 
+                                                "Simulation Study (2): Mean trends" = "dt_mean", 
+                                                "Simulation Study (3): Misspecified temporal networks" = "dt_rt",
+                                                "Simulation Study (4): Misspecified contemporaneous networks" = "dt_rc",
+                                                "Simulation Study (5): Non-stationary temporal networks" = "dt_nt",
+                                                "Simulation Study (6): Non-stationary contemporaneous networks" = "dt_nc")),
+                        
+                        # *--- choose true model ---
+                        selectInput("true_model", "Choose the true model setup",
+                                    choices = c("nei = 1 & propPos = 0.5",
+                                                "nei = 1 & propPos = 0.8",
+                                                "nei = 2 & propPos = 0.5")),
+                        
+                        # * --- choose model type --- 
+                        selectInput("model_type", "Choose the type of model",
+                                      choices = c("Confirmatory", "Saturated", "Pruned")),
+                        
+                        # *--- choose sample characteristics --- 
+                        conditionalPanel(
+                          condition = "input.dt_mis_condition != 'dt_null'",
+                          selectInput("n_nodes", "Choose the number of nodes",
+                                      choices = c("6 nodes" = "p = 6",
+                                                  "12 nodes" = "p = 12"))),
+                        
+                        # *--- select the cutoff values ---
+                        tags$div(tags$b("Set cutoff values:")),
+                        
+                        slider_set_up("NFI", .95), # inputId is paste0(id, "_cut)
+                        slider_set_up("PNFI", .80),
+                        slider_set_up("TLI", .95),
+                        slider_set_up("RFI", .95),
+                        slider_set_up("IFI", .95),
+                        slider_set_up("RNI", .95),
+                        slider_set_up("CFI", .95),
+                        slider_set_up("RMSEA", .05),
+                        
+                      ), # end sidebar panel
+                      
+                      # *--- plots display ---
+                      mainPanel(
+                        plotOutput("line_plot")
+                      ) # end main panel
+                    ) # end sidebar layout
+           ), # end tab Line Plots
+           
+           tabPanel("ANCOVA tables",
+                    sidebarLayout(
+                      sidebarPanel(
+                        actionButton("make_ANOVA_table", "Create table"),
+                        hr(),
+
+                        # *--- choose dataset ---
+                        selectInput("dt_mis_condition", "Choose the misspecification condition",
+                                    choices = c("Simulation Study (2): Mean trends" = "dt_mean",
+                                                "Simulation Study (3): Misspecified temporal networks" = "dt_rt",
+                                                "Simulation Study (4): Misspecified contemporaneous networks" = "dt_rc",
+                                                "Simulation Study (5): Non-stationary temporal networks" = "dt_nt",
+                                                "Simulation Study (6): Non-stationary contemporaneous networks" = "dt_nc")),
+
+                        # *--- choose true model ---
+                        selectInput("true_model", "Choose the true model setup",
+                                    choices = c("nei = 1 & propPos = 0.5",
+                                                "nei = 1 & propPos = 0.8",
+                                                "nei = 2 & propPos = 0.5")),
+
+                      ), # end sidebar panel
+
+                      # *--- plots display ---
+                      mainPanel(
+                        tableOutput("ancova_table")
+                      ) # end main panel
+                    ) # end sidebar layout
+           ) # end tab ANCOVA tables
+
+)
